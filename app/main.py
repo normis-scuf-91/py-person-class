4e4de577b2feb078358c72bcda248ed7e8222a3d
Normis-Skuf <test-email@mail.com>
2026-02-24 00:19:29 +0200

    developed person class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result = [Person(ind.get("name"), ind.get("age")) for ind in people]
+    for pers in people:
+        part = pers.get("wife")
+        if part:
+            Person.people[pers["name"]].wife = Person.people[part]
+        part = pers.get("husband")
+        if part:
+            Person.people[pers["name"]].husband = Person.people[part]
+    return result
